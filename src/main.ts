254d195ed969745d0af6fecca20c5bafa8c53009
Eugene <inhuman.en@gmail.com>
2018-04-19 16:29:41 +0300

    fix: fixed pixelation

--- src/main.ts
+++ src/main.ts
@@ -19,20 +19,22 @@ import { OrbitControls } from 'three-orbitcontrols-ts';
 import { starfield } from './objects';
 import * as Shaders from './shaders';
 window.onload = function() {
+    let textureLoader = new TextureLoader();
     let scene = new Scene();
-    let camera = new PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-
+    let camera = new PerspectiveCamera(80, window.innerWidth / window.innerHeight, 0.1, 2000);
     let controls = new OrbitControls(camera);
+    let renderer = new WebGLRenderer({ antialias: true, alpha: false });
 
-    let renderer = new WebGLRenderer({ antialias: true });
+    // scene.background = textureLoader.load('/assets/textures/mw.jpeg');
     renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
 
     document.body.appendChild(renderer.domElement);
 
     renderer.autoClear = false;
 
-    let geometry = new SphereGeometry(2, 300, 300);
-    let textureLoader = new TextureLoader();
+    let geometry = new SphereGeometry(2.5, 300, 300);
     let clock = new Clock();
 
     let uniforms = {
@@ -87,8 +89,9 @@ window.onload = function() {
 
         camera.updateProjectionMatrix();
         camera.lookAt(scene.position);
-
         renderer.setSize(window.innerWidth, window.innerHeight);
+
+        
 
         // composer.reset();
     };
